833fb6608ce9af5bc88ec797fcf1536de9cd2fd3
brandlju <julius.brandl@gmx.de>
2020-10-17 17:12:19 +0200

    add cell and grid init try to fix check_collision

--- src/model/model.py
+++ src/model/model.py
@@ -2,15 +2,15 @@ from mesa import Model
 from mesa.time import SimultaneousActivation
 from mesa.space import MultiGrid
 import numpy as np
-from src.model.agents import SpeedAgent, AgentTrace, RandomAgent
-from src.model.utils import Direction
+from src.model.agents import SpeedAgent, AgentTrace, RandomAgent, AgentTraceCollision
+from src.model.utils import Direction, compare_grid_with_cells
 
 
 class SpeedModel(Model):
     """
     Model of the game "Speed". This class controls the execution of the simulation.
     """
-    def __init__(self, width, height, nb_agents, initial_agents_params=None, agent_classes=None, data_collector=None,
+    def __init__(self, width, height, nb_agents, cells=None, initial_agents_params=None, agent_classes=None, data_collector=None,
                  verbose=True):
         """
         :param initial_agents_params: A list of dictionaries containing initialization parameters for agents
@@ -27,11 +27,12 @@ class SpeedModel(Model):
             initial_agents_params = [{} for i in range(nb_agents)]
 
         self.schedule = SimultaneousActivation(self)
+
         self.grid = MultiGrid(width, height, True)
         # width and height are swapped since height is rows and width is columns
         # an alternative to this representation would be to transpose cells everytime it is exposed
         # but that could be inefficient
-        self.cells = np.zeros((height, width))
+        self.cells = np.zeros((height, width), dtype="int")
 
         # Init initial agents
         self.speed_agents = []
@@ -53,6 +54,32 @@ class SpeedModel(Model):
             self.speed_agents.append(agent)
             self.active_speed_agents.append(agent)
 
+        if cells is not None:
+            self.init_cells_and_grid(cells)
+
+    def init_cells_and_grid(self, cells):
+        self.cells = np.array(cells)
+        # add traces to grid
+        for y in range(self.cells.shape[0]):
+            for x in range(self.cells.shape[1]):
+                # cell is not occupied
+                if self.cells[y, x] == 0:
+                    continue
+                # cell is occupied by a collision
+                elif self.cells[y, x] == -1:
+                    agent = AgentTraceCollision(self, (x, y))
+                    self.add_agent(agent)
+                # cell is occupied by head or trace
+                else:
+                    # head of the agent is already a entry in self.grid
+                    if len(self.grid.get_cell_list_contents((x, y))) != 0:
+                        continue
+                    else:
+                        # add trace
+                        agent = AgentTrace(self, (x, y), self.speed_agents[self.cells[y, x]-1])  # get agent based on id
+                        self.add_agent(agent)
+        compare_grid_with_cells(self)
+
     def step(self):
         """
         Computes one iteration of the model.
@@ -60,16 +87,61 @@ class SpeedModel(Model):
         """
         if self.data_collector:
             self.data_collector.collect(self)
-        self.schedule.step()
+
         self.check_collisions()
         self.check_game_finished()
+        print(self.schedule.steps)
+        compare_grid_with_cells(self)
+        self.schedule.step()  # changed order to match original game
+
 
     def check_collisions(self):
         """
         Checks every active agent for collisions with traces or other agents. Colliding agents are eliminated.
+        TODO: NOT WORKING CORRECTLY! agent with smaller id is set inactive even if just his tail got hit.
+         Maybe add Timestamp to AgentTrace to track witch agent visited the cell first.
         :return: None
         """
+
+        def set_inactive(agent):
+            print(f"Agent {agent.unique_id} lost.")
+            if isinstance(agent, SpeedAgent):
+                agent.set_inactive()
+            else:
+                agent.origin.set_inactive()
+
         # the agent is eliminated if its trace (of the last step) overlaps with any other traces
+        for speed_agent in self.active_speed_agents:
+            for t in speed_agent.trace:
+                cell_contents = self.grid.get_cell_list_contents(t)
+                # if three agents are in one cell all of them must be heads of speedagents
+                if len(cell_contents) > 2:
+                    for content in cell_contents:
+                        set_inactive(content)
+                elif len(cell_contents) > 1:
+                    visit_step = []
+                    for content in cell_contents:
+                        if isinstance(content, SpeedAgent):
+                            visit_step.append(self.schedule.steps)
+                        else:
+                            visit_step.append(content.creation_step)
+                    if visit_step[0] == visit_step[1]:
+                        set_inactive(cell_contents[0])
+                        set_inactive(cell_contents[1])
+                    elif visit_step[0] > visit_step[1]:
+                        set_inactive(cell_contents[0])
+                    else:
+                        set_inactive(cell_contents[1])
+
+
+
+                    # swapped position args since cells has the format (height, width)
+                    for content in cell_contents:
+                        self.remove_agent(content)
+                    self.add_agent(AgentTraceCollision(self, t))
+
+                    self.cells[t[1], t[0]] = -1
+        """
         for agent in self.active_speed_agents:
             for t in agent.trace:
                 cell_contents = self.grid.get_cell_list_contents(t)
@@ -77,6 +149,8 @@ class SpeedModel(Model):
                     agent.set_inactive()
                     # swapped position args since cells has the format (height, width)
                     self.cells[t[1], t[0]] = -1
+                    
+        """
 
     def check_game_finished(self):
         """
@@ -111,6 +185,8 @@ class SpeedModel(Model):
         pos = (agent.pos[1], agent.pos[0])
         if isinstance(agent, SpeedAgent):
             self.cells[pos] = agent.unique_id
+        elif isinstance(agent, AgentTraceCollision):
+            self.cells[pos] = -1
         elif isinstance(agent, AgentTrace):
             self.cells[pos] = agent.origin.unique_id
 
