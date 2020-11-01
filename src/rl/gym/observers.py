021e259c7859f9bc4932aa40d452270326339a01
demmlema <maximilian.demmler@student.uni-augsburg.de>
2020-11-01 23:13:34 +0100

    Added pure image based CNN and observer

--- src/rl/gym/observers.py
+++ src/rl/gym/observers.py
@@ -57,7 +57,7 @@ class GlobalObserver:
         ])
 
 
-class GlobalImageObserver:
+class GlobalMixedImageObserver:
 
     def __init__(self, nb_agents, width, height):
         self.nb_agents = nb_agents
@@ -95,7 +95,10 @@ class GlobalImageObserver:
             player = state["players"][player_id]
             player_inf.append(player["x"] / height)
             player_inf.append(player["y"] / width)
-            player_inf.append(Direction[player["direction"].upper()].value / 4)
+            # one hot for direction
+            direction = np.zeros(4)
+            direction[Direction[player["direction"].upper()].value] = 1
+            player_inf.extend(direction)
             player_inf.append(player["speed"] / 10)
             player_inf.append(1 if player["active"] else 0)
 
@@ -113,3 +116,71 @@ class GlobalImageObserver:
         #img.show()
 
         return pixels
+
+
+class GlobalImageObserver:
+
+    def __init__(self, nb_agents, width, height):
+        self.nb_agents = nb_agents
+        self.width = width
+        self.height = height
+        lows = np.zeros((height, width, 3))
+        highs = np.full((height, width, 3), 255)
+        self.observation_space = spaces.Box(low=lows, high=highs, shape=(height, width, 3), dtype='uint8')
+
+    def prepared_state(self, state, agent_id, step_counter):
+        # format: [R, G, B, A] image with R=Obstacles, G=Opponents, B=Self, A=Round_Counter
+        # every input is normalized to [0, 255]
+        original_cells = state["cells"]
+        width = state["width"]
+        height = state["height"]
+
+        cells = np.zeros((height, width, 3), dtype='uint8')
+        for x in range(width):
+            for y in range(height):
+                # set every cell to 0 (empty) or 1 (occupied)
+                # it is important to create a new cells array in order to not overwrite the original one
+                if original_cells[y, x] == 0:
+                    cells[y, x, 0] = 0
+                else:
+                    cells[y, x, 0] = 255
+
+                # put the 6 round counter on the 4th layer
+                #cells[y, x, 3] = 255 #int((step_counter % 6) / 5 * 255)
+
+        # player information is put into layers 2 and 3 (g, b)
+        other_player_ids = list(state["players"].keys())
+        other_player_ids.remove(str(agent_id))
+        for player_id in other_player_ids:
+            player = state["players"][player_id]
+            if player["active"]:
+                self.add_player_to_layer(cells, 1, player)
+        self.add_player_to_layer(cells, 2, state["players"][str(agent_id)])
+
+        pixels = cv2.resize(cells, dsize=(self.height, self.width), interpolation=cv2.INTER_NEAREST)
+
+        # img = Image.fromarray(pixels, 'RGB')
+        # img.show()
+        return pixels
+
+    @staticmethod
+    def add_player_to_layer(cells, layer_id, player):
+        x, y = player["x"], player["y"]
+        if 0 < player["y"] >= cells.shape[0] or 0 < player["x"] >= cells.shape[1]:
+            return
+        # 127 to mark the player + speed indication
+        cells[y, x, layer_id] += 127 + player["speed"] / 10 * 64
+
+        direction = Direction[player["direction"].upper()]
+        if direction == Direction.UP:
+            x -= 1
+        elif direction == Direction.DOWN:
+            x += 1
+        elif direction == Direction.LEFT:
+            y -= 1
+        elif direction == Direction.RIGHT:
+            y += 1
+        x = max(min(x, cells.shape[1] - 1), 0)
+        y = max(min(y, cells.shape[0] - 1), 0)
+        # indicate direction with 16 (16, so that the value on a pixel can at most add up to 255)
+        cells[y, x, layer_id] += 64
--- src/rl/scripts/train_dqn_vs_heuristics.py
+++ src/rl/scripts/train_dqn_vs_heuristics.py
@@ -1,6 +1,6 @@
 from stable_baselines import DQN
 from src.rl.gym.environments import HeuristicsSpeedEnv
-from src.rl.gym.observers import GlobalObserver, GlobalImageObserver
+from src.rl.gym.observers import GlobalObserver, GlobalMixedImageObserver, GlobalImageObserver
 from src.model.agents import RandomAgent, AgentDummy, OneStepSurvivalAgent
 from src.rl.policies import nature_cnn_mlp_mix
 
@@ -13,10 +13,10 @@ erm_size = 100_000
 
 # init environment and agent
 policy_kwargs = dict(cnn_extractor=nature_cnn_mlp_mix)
-env = HeuristicsSpeedEnv(width=16, height=16, observer=GlobalImageObserver(nb_agents=2, width=16, height=16),
+env = HeuristicsSpeedEnv(width=16, height=16, observer=GlobalMixedImageObserver(nb_agents=2, width=64, height=64),
                          agent_classes=[AgentDummy, OneStepSurvivalAgent])
-model = DQN("CnnPolicy", env, verbose=1, buffer_size=erm_size, target_network_update_freq=50, exploration_fraction=0.5,
-            policy_kwargs=policy_kwargs)
+model = DQN("CnnPolicy", env, verbose=1, buffer_size=erm_size, target_network_update_freq=50, exploration_fraction=0.3,
+            policy_kwargs=policy_kwargs, tensorboard_log="../../../res/rl/tensorboard/")
 
 # train and save
 model.learn(total_timesteps=nb_training_steps)
