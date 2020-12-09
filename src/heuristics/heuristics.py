b21e60d02957c4fd967f3d3293a36d4626ff956e
demmlema <maximilian.demmler@student.uni-augsburg.de>
2020-12-09 13:39:16 +0100

    Weighted evaluation

--- src/heuristics/heuristics.py
+++ src/heuristics/heuristics.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 
 def multi_minimax(depth, game_state, super_pruning=False, use_voronoi=True):
+    max_depth = depth
     pruning_threshold = -1
     model = state_to_model(game_state)
     own_id = game_state["you"]
@@ -27,7 +28,8 @@ def multi_minimax(depth, game_state, super_pruning=False, use_voronoi=True):
         for opponent_id in min_player_ids:
             opponent = model.get_agent_by_id(opponent_id)
 
-            min_move = min(min_move, minimax(max_player, opponent, depth-1, alpha, beta, False, model, node, use_voronoi))
+            min_move = min(min_move, minimax(max_player, opponent, depth-1, max_depth, alpha, beta, False, model, node,
+                                             use_voronoi))
             # for pre, fill, node in RenderTree(start_node):
             #     print("%s%s" % (pre, node.name))
 
@@ -56,9 +58,10 @@ def update_game_state(model, agent, action):
     model.step_specific_agent(agent)
 
 
-def minimax(max_player, min_player, depth, alpha, beta, is_max, model, last_parent_node, use_voronoi):
+def minimax(max_player, min_player, depth, max_depth, alpha, beta, is_max, model, last_parent_node, use_voronoi):
     if depth == 0 or not max_player.active or not model.running:
-        return evaluate_position(model_to_json(model, trace_aware=True), max_player, min_player, depth, use_voronoi)
+        return evaluate_position(model_to_json(model, trace_aware=True), max_player, min_player, depth, max_depth,
+                                 use_voronoi)
     if is_max:
         max_move = float("-inf")
         pre_state = model_to_json(model, trace_aware=True)
@@ -66,7 +69,8 @@ def minimax(max_player, min_player, depth, alpha, beta, is_max, model, last_pare
             update_game_state(model, max_player, action)
             node = Node(str(min_player.unique_id), parent=last_parent_node)
 
-            max_move = max(max_move, minimax(max_player, min_player, depth-1, alpha, beta, False, model, node, use_voronoi))
+            max_move = max(max_move, minimax(max_player, min_player, depth-1, max_depth, alpha, beta, False, model,
+                                             node, use_voronoi))
 
             model = state_to_model(pre_state)
             own_id = max_player.unique_id
@@ -85,7 +89,8 @@ def minimax(max_player, min_player, depth, alpha, beta, is_max, model, last_pare
             update_game_state(model, min_player, action)
             node = Node(str(max_player.unique_id), parent=last_parent_node)
 
-            min_move = min(min_move, minimax(max_player, min_player, depth-1, alpha, beta, True, model, node, use_voronoi))
+            min_move = min(min_move, minimax(max_player, min_player, depth-1, max_depth, alpha, beta, True, model, node,
+                                             use_voronoi))
 
             model = state_to_model(pre_state, trace_aware=True)
             own_id = max_player.unique_id
@@ -99,18 +104,35 @@ def minimax(max_player, min_player, depth, alpha, beta, is_max, model, last_pare
         return min_move
 
 
-def evaluate_position(state, max_player, min_player, depth, use_voronoi):
+def evaluate_position(state, max_player, min_player, depth, max_depth, use_voronoi):
     if not use_voronoi:
         if max_player.active and not min_player.active:
             return float("inf")
         else:
             return -1 * depth
     else:
+        # weights - all non-weighted evaluation values should be in [-1, 1]
+        # using very large weight gaps is effectively like prioritization
+        kill_weight = float("inf")
+        death_weight = 1000
+        voronoi_region_weight = 1
+        territory_bonus_weight = 0.001  # only used to decide between positions with equal voronoi region evaluation
+
         model = state_to_model(state)
         if max_player.active and not min_player.active:
-            return float("inf")
+            # TODO: Maybe it is non-optimal to kill an opponent in a 1vx situation (its optimal in 1v1).
+            #       It also makes a difference which opponent is killed if multiple opponents can be killed in a 1vx
+            #       (e.g. kill the one with a larger voronoi region)
+            return kill_weight
         elif not max_player.active:
-            return -1000 * depth
+            # TODO: Detect situations where the game is lost if we try to survive but we can force a kamikaze draw.
+            #       At the moment the agent would always try to survive one more step and would only kamikaze if death
+            #       is inevitable.
+            death_eval = -depth
+            if not min_player.active:
+                # kamikaze is better than just dying without killing someone else
+                death_eval += 0.1
+            return death_eval / max_depth * death_weight
 
         voronoi_cells, voronoi_counter = speed_one_voronoi(model)
         nb_cells = float(model.width * model.height)   # for normalization
@@ -118,14 +140,14 @@ def evaluate_position(state, max_player, min_player, depth, use_voronoi):
         # voronoi region size comparison
         max_player_size = voronoi_counter[max_player.unique_id] if max_player.unique_id in voronoi_counter.keys() else 0
         min_player_size = voronoi_counter[min_player.unique_id] if min_player.unique_id in voronoi_counter.keys() else 0
-        voronoi_region_points = (max_player_size - min_player_size) / nb_cells
+        voronoi_region_points = (max_player_size - min_player_size) / nb_cells * voronoi_region_weight
 
         territory_bonus = 0
         for x in range(model.width):
             for y in range(model.height):
                 if voronoi_cells[y, x, 0] == max_player.unique_id:
                     territory_bonus += add_territory_bonus(model, x, y)
-        territory_bonus /= nb_cells
+        territory_bonus *= territory_bonus_weight / nb_cells
 
         return voronoi_region_points + territory_bonus
 
