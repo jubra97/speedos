1981d85bd3eac058a65cdf37616577ac6faf9e60
demmlema <maximilian.demmler@student.uni-augsburg.de>
2021-01-12 22:09:49 +0100

    evaluation

--- src/evaluation/run.py
+++ src/evaluation/run.py
@@ -1,11 +1,19 @@
-from src.core.agents import NStepSurvivalAgent
+from multiprocessing.spawn import freeze_support
+
+from src.core.agents import MultiMinimaxAgent, VoronoiAgent, SlidingWindowVoronoiAgent, ClosestOpponentsVoronoiAgent
 from src.evaluation.utils import Evaluator
 
-model_params = {
-    "width": 20,
-    "height": 20,
-    "nb_agents": 3,
-    "agent_classes": [NStepSurvivalAgent, NStepSurvivalAgent, NStepSurvivalAgent],
-}
-evaluator = Evaluator(model_params)
-evaluator.fair_start_evaluate(3, save=True)
+if __name__ == '__main__':
+    freeze_support()
+    for i in range(100):
+        model_params = {
+            "width": 60,
+            "height": 60,
+            "nb_agents": 5,
+            "agent_classes": [MultiMinimaxAgent, VoronoiAgent, SlidingWindowVoronoiAgent, SlidingWindowVoronoiAgent,
+                              ClosestOpponentsVoronoiAgent],
+            "initial_agents_params": [{}, {}, {"min_sliding_window_size": 20, "sliding_window_size_offset": 5},
+                                      {"min_sliding_window_size": 15, "sliding_window_size_offset": 3}, {}]
+        }
+        evaluator = Evaluator(model_params)
+        evaluator.fair_start_evaluate(5, save=True, random_move_time=True)
--- src/evaluation/utils.py
+++ src/evaluation/utils.py
@@ -15,23 +15,38 @@ class Evaluator:
         self.model_params = model_params
         self.parameter_settings_info = parameter_settings_info
 
+        if "initial_agents_params" not in self.model_params:
+            self.model_params["initial_agents_params"] = [{} for _ in range(self.model_params["nb_agents"])]
         self.model = SpeedModel(**model_params)
         self.win_table = None
         self.elimination_step_table = None
+        self.placement_table = None
         self.elimination_action_table = None
         self.agent_independent_table = None
 
-    def evaluate(self, repetitions, seeds=None, show=True, save=False):
+    def evaluate(self, repetitions, seeds=None, show=True, save=False, random_move_time=False):
         self._init_tables(repetitions)
         for rep in range(repetitions):
+            if random_move_time:
+                move_time = np.random.uniform(5, 15)
+                for i in range(self.model_params["nb_agents"]):
+                    self.model_params["initial_agents_params"][i]["time_for_move"] = move_time
             self.model = SpeedModel(**self.model_params)
             if seeds is not None:
                 self.model.reset_randomizer(seeds[rep])
-            self.model.run_model()
+            while self.model.running:
+                active_agent_ids = list(map(lambda x: x.unique_id, self.model.active_speed_agents))
+                self.model.step()
+                new_active_agent_ids = list(map(lambda x: x.unique_id, self.model.active_speed_agents))
+                active_agents_ids_disjunction = list(set(active_agent_ids) - set(new_active_agent_ids))
+                for unique_id in active_agents_ids_disjunction:
+                    self.placement_table[unique_id - 1, rep] += len(self.model.active_speed_agents) + 1
+            for winner in self.model.active_speed_agents:
+                self.placement_table[winner.unique_id - 1, rep] += 1
             self._update_tables(rep)
         self._process_results(repetitions, show, save)
 
-    def fair_start_evaluate(self, repetitions, seeds=None, show=True, save=False):
+    def fair_start_evaluate(self, repetitions, seeds=None, show=True, save=False, random_move_time=False):
         if repetitions % self.model_params["nb_agents"]:
             raise ValueError("Repetitions must be a multiple of nb_agents")
         else:
@@ -47,12 +62,26 @@ class Evaluator:
                 args = [{"pos": start_pos[j % self.model_params["nb_agents"]],
                          "direction": start_dir[j % self.model_params["nb_agents"]]} for j in
                         range(i, self.model_params["nb_agents"] + i)]
-                self.model_params["initial_agents_params"] = args
+                for j in range(self.model_params["nb_agents"]):
+                    self.model_params["initial_agents_params"][i]["pos"] = args[i]["pos"]
+                    self.model_params["initial_agents_params"][i]["direction"] = args[i]["direction"]
 
+                if random_move_time:
+                    move_time = np.random.uniform(1, 2)
+                    for j in range(self.model_params["nb_agents"]):
+                        self.model_params["initial_agents_params"][j]["time_for_move"] = move_time
                 self.model = SpeedModel(**self.model_params)
                 if seeds is not None:
                     self.model.reset_randomizer(seeds[rep])
-                self.model.run_model()
+                while self.model.running:
+                    active_agent_ids = list(map(lambda x: x.unique_id, self.model.active_speed_agents))
+                    self.model.step()
+                    new_active_agent_ids = list(map(lambda x: x.unique_id, self.model.active_speed_agents))
+                    active_agents_ids_disjunction = list(set(active_agent_ids) - set(new_active_agent_ids))
+                    for unique_id in active_agents_ids_disjunction:
+                        self.placement_table[unique_id - 1, rep] += len(self.model.active_speed_agents) + 1
+                for winner in self.model.active_speed_agents:
+                    self.placement_table[winner.unique_id - 1, rep] += 1
                 self._update_tables(rep * self.model_params["nb_agents"] + i)
         self._process_results(repetitions, show, save)
 
@@ -77,7 +106,13 @@ class Evaluator:
                                              columns=["EA Left", "EA Right", "EA Slow Down",
                                                       "EA Speed Up", "EA Change Nothing"])
 
-        agent_table = win_df.join(elimination_step_df).join(elimination_action_df)
+        placement_data = {"Placement Mean": [], "Placement Std": []}
+        for agent_data in self.placement_table:
+            placement_data["Placement Mean"].append(np.mean(agent_data))
+            placement_data["Placement Std"].append(np.mean(agent_data))
+        placement_df = pd.DataFrame(placement_data, index=index)
+
+        agent_table = win_df.join(elimination_step_df).join(elimination_action_df).join(placement_df)
 
         self.agent_independent_table[1] *= 100 / (self.model.width * self.model.height)  # convert to percentages
         agent_independent_df = pd.DataFrame({
@@ -119,6 +154,7 @@ class Evaluator:
 
     def _init_tables(self, repetitions):
         self.win_table = np.zeros((self.model.nb_agents, 3))
+        self.placement_table = np.zeros((self.model.nb_agents, repetitions))
         self.elimination_step_table = np.zeros((self.model.nb_agents, repetitions))
         self.elimination_action_table = np.zeros((self.model.nb_agents, 5), dtype=np.int)
         self.agent_independent_table = np.empty((2, repetitions))
