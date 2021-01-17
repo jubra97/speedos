58af0907cff441410f22225f9895085ddaa2951b
Julius Brandl <julius.brandl97@gmail.com>
2021-01-17 19:28:48 +0100

    update run_docker.py update run.py

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -1,7 +1,7 @@
 
-from src.core.agents import ParallelVoronoiAgent
+from src.core.agents import VoronoiAgent, SlidingWindowVoronoiAgent
 from src.core.model import SpeedModel
 
 if __name__ == "__main__":
-    model = SpeedModel(6, 6, 2, agent_classes=[ParallelVoronoiAgent, ParallelVoronoiAgent])
+    model = SpeedModel(6, 6, 2, agent_classes=[VoronoiAgent, SlidingWindowVoronoiAgent])
     model.run_model()
